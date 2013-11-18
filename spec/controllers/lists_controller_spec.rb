47b6b369d9d7fd41769d0f279fffbc2845299c48
Steveo1485 <stevennugent@gmail.com>
2013-11-18 15:19:26 -0800

    Add new action on lists controller and new view for lists

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -3,11 +3,22 @@ require 'spec_helper'
 describe ListsController do
   let!(:list) { List.new(item1: "Ninja Turtle")}
 
+  it "#new" do
+    get :new
+    response.status.should eq(200)
+  end
+
   context "#create" do
     it "creates a new list with valid params" do
       expect {
         post :create, list: { item1: list.item1 }
       }.to change{ List.count }.by(1)
+    end
+
+    it "does not create a new list with invalid params" do
+      expect {
+        post :create, list: { item1: "" }
+      }.to_not change{ List.count }
     end
   end
 end
