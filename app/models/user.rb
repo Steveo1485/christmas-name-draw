003589dfcf47b3459c8c0891d24ebcde8362018e
Steven Nugent <stevennugentdev@gmail.com>
2013-11-20 14:27:46 -0800

    Add has_one list association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  has_one :list
+
   validates_presence_of :first_name
   validates_uniqueness_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
   validates_presence_of :password, :on => :create
+  
   has_secure_password
 end
