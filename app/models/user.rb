0e0005919b475ca5e8d836b2de0393a9d4f2ce46
Steven Nugent <stevennugentdev@gmail.com>
2013-11-20 14:15:57 -0800

    Add password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
+  validates_presence_of :password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131120221110) do
+ActiveRecord::Schema.define(:version => 20131120221440) do
 
   create_table "lists", :force => true do |t|
     t.integer "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20131120221110) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "password"
   end
 
 end
