59443260acccba88cbb63b797d4b33ea65b65227
Steven Nugent <stevennugentdev@gmail.com>
2013-11-20 18:12:36 -0800

    Add create action for Users Controller, testing for valid creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ChristmasGiftExchange::Application.routes.draw do
   root :to => 'lists#index'
 
-  resources :users, only: [:index, :new] do
+  resources :users, only: [:index, :new, :create] do
     resources :list, only: [:index, :new, :create, :show], controller: "lists"
   end
 end
