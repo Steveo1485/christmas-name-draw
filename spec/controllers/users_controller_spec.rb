fc1b5e41c51e63fa5477ab04577778c27368171d
Steven Nugent <stevennugentdev@gmail.com>
2013-11-20 19:37:35 -0800

    Add controller test for valid user sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe UsersController do
   let!(:user) { User.new(first_name: "Steven", last_name: "Nugent", email: "steven@steven.com", password: "password")}
+  let!(:valid_user) { User.new(first_name: "Derek", last_name: "Dragseth", email: "derek@derek.com", password: "password")}
 
   it "#index" do
     get :index
@@ -30,6 +31,13 @@ describe UsersController do
                               email: user.email,
                               password: user.password}
       }.to_not change{ User.count }
+    end
+  end
+
+  context "#sign_in" do
+    it "signs in user with valid credentials" do
+      post :sign_in, user: {email: valid_user.email, password: valid_user.password}
+      expect(session[:user_id]).to eq(valid_user.id)
     end
   end
 end
