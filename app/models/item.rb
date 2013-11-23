9b3863d214445b25ee51f1b8dd7151db657639a2
Steven Nugent <stevennugentdev@gmail.com>
2013-11-23 12:48:44 -0800

    Add create action to items controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
+  attr_accessible :item
   belongs_to :list
   validates_presence_of :item
 end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe ItemsController do
   let!(:user) { User.create(first_name: "Steven", last_name: "Nugent", email: "steven@steven.com", password: "password")}
-  let!(:list) { List.create(item1: "Ninja Turtle", user_id: user.id)}
+  let!(:list) { List.create(user_id: user.id)}
   
   context "#create" do
-    xit "creates a new item with valid params" do
+    it "creates a new item with valid params" do
       expect {
-        post :create, user_id: user.id, list_id:  { item1: list.item1, user_id: user.id }
-      }.to change{ List.count }.by(1)
+        post :create, user_id: user.id, list_id: list.id, item: { item: "Ninja Turtle" }
+      }.to change{ Item.count }.by(1)
     end
 
     xit "does not create a new list with invalid params" do
