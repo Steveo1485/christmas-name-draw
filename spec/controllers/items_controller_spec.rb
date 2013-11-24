bd4cd6bd456954e63ed8e04fce348c3629807b28
Steven Nugent <stevennugentdev@gmail.com>
2013-11-23 16:14:48 -0800

    User can delete an item

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe ItemsController do
   let!(:user) { User.create(first_name: "Steven", last_name: "Nugent", email: "steven@steven.com", password: "password")}
   let!(:list) { List.create(user_id: user.id)}
+  let!(:item) { Item.create(item: "Ninja Turtle", list_id: list.id)}
   
   context "#create" do
     it "creates a new item with valid params" do
@@ -16,5 +17,11 @@ describe ItemsController do
         post :create, user_id: user.id, list_id: list.id, item: { item: "" }
       }.to_not change{ List.count }
     end
+  end
+
+  it "#destroy" do
+    expect {
+      delete :destroy, user_id: user.id, list_id: list.id, id: item.id
+    }.to change{ Item.count }.by(-1)
   end
 end
