9697a8e3a5b35018b557a7404f46ee30877f86b2
Steven Nugent <stevennugentdev@gmail.com>
2013-11-23 21:01:50 -0800

    Add redirect on root path if user is already signed in

--- spec/features/user_can_sign_up_and_log_in_spec.rb
+++ spec/features/user_can_sign_up_and_log_in_spec.rb
@@ -20,4 +20,13 @@ describe "User" do
     click_button("Sign In")
     expect(page).to have_content("Welcome, #{user.first_name}!")
   end
+
+  it "should redirect root path if previously signed in" do
+    visit root_path
+    fill_in "sign_in_email", with: user.email
+    fill_in "sign_in_password", with: user.password
+    click_button("Sign In")
+    visit root_path
+    expect(page).to have_content("Welcome, #{user.first_name}!")
+  end
 end
