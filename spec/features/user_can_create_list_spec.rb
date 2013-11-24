e6897fe370ccc37d0bc399ca071e3457a1a6dc26
Steven Nugent <stevennugentdev@gmail.com>
2013-11-23 16:41:01 -0800

    Add test for adding an item

--- spec/features/user_can_create_list_spec.rb
+++ spec/features/user_can_create_list_spec.rb
@@ -3,30 +3,23 @@ require 'spec_helper'
 describe "User" do
   let!(:user) { User.create(first_name: "Steven", last_name: "Nugent", email: "steven@steven.com", password: "password")}
   
-  it "can sign up" do
-    visit root_path
-    fill_in "user_first_name", with: "Stephanie"
-    fill_in "user_last_name", with: "King"
-    fill_in "user_email", with: "stephanie@stephanie.com"
-    fill_in "user_password", with: "password"
-    click_button("Sign Up")
-    expect(page).to have_content("Welcome, Stephanie!")
-  end
-
-  it "can log in" do
-    visit root_path
-    fill_in "sign_in_email", with: "steven@steven.com"
-    fill_in "sign_in_password", with: "password"
-    click_button("Sign In")
-    expect(page).to have_content("Welcome, Steven!")
-  end
-
   it "can create a new list" do
     visit root_path
-    fill_in "sign_in_email", with: "steven@steven.com"
-    fill_in "sign_in_password", with: "password"
+    fill_in "sign_in_email", with: user.email
+    fill_in "sign_in_password", with: user.password
     click_button("Sign In")
     click_link "Create Christmas List"
     expect(page).to have_content("Item")
+  end
+
+  it "can add item to list" do
+    visit root_path
+    fill_in "sign_in_email", with: user.email
+    fill_in "sign_in_password", with: user.password
+    click_button("Sign In")
+    click_link "Create Christmas List"
+    fill_in "item_item", with: "Ninja Turtle"
+    click_button("Add Item")
+    expect(page).to have_content("Ninja Turtle")
   end
 end
---
+++ spec/features/user_can_sign_up_and_log_in_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "User" do
+  let!(:user) { User.create(first_name: "Steven", last_name: "Nugent", email: "steven@steven.com", password: "password")}
+  
+  it "can sign up" do
+    visit root_path
+    fill_in "user_first_name", with: "Stephanie"
+    fill_in "user_last_name", with: "King"
+    fill_in "user_email", with: "stephanie@stephanie.com"
+    fill_in "user_password", with: "password"
+    click_button("Sign Up")
+    expect(page).to have_content("Welcome, Stephanie!")
+  end
+
+  it "can log in" do
+    visit root_path
+    fill_in "sign_in_email", with: user.email
+    fill_in "sign_in_password", with: user.password
+    click_button("Sign In")
+    expect(page).to have_content("Welcome, #{user.first_name}!")
+  end
+end
