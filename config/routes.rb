757866a7d4f4b033879e036d3f37054418985603
Steven Nugent <stevennugentdev@gmail.com>
2013-11-23 16:54:16 -0800

    Remove unused users index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ChristmasGiftExchange::Application.routes.draw do
   root :to => 'users#new'
 
-  resources :users, only: [:index, :new, :create] do
+  resources :users, only: [:new, :create] do
     resources :lists, only: [:index, :create], controller: "lists" do
       resources :items, only: [:create, :destroy]
     end
