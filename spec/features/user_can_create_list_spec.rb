9497c6835230157825be3b0457425503865e12c5
Steven Nugent <stevennugentdev@gmail.com>
2013-11-23 16:42:27 -0800

    Add feature test for removing item from list

--- spec/features/user_can_create_list_spec.rb
+++ spec/features/user_can_create_list_spec.rb
@@ -22,4 +22,16 @@ describe "User" do
     click_button("Add Item")
     expect(page).to have_content("Ninja Turtle")
   end
+
+  it "can remove an item from a list" do
+    visit root_path
+    fill_in "sign_in_email", with: user.email
+    fill_in "sign_in_password", with: user.password
+    click_button("Sign In")
+    click_link "Create Christmas List"
+    fill_in "item_item", with: "Ninja Turtle"
+    click_button("Add Item")
+    click_link("Remove from list")
+    expect(page).not_to have_content("Ninja Turtle")
+  end
 end
