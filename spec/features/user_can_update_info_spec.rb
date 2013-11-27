e49d7db0487150e7d5c07518599b988fa8b5c997
Steven Nugent <stevennugentdev@gmail.com>
2013-11-27 14:44:05 -0800

    Style user show view, add links to show view, add update info feature test

---
+++ spec/features/user_can_update_info_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "User" do
+  let!(:user) { User.create(first_name: "Steven", last_name: "Nugent", email: "steven@steven.com", password: "password", family_group: "Nugent/Lim/Saito")}
+  
+  it "update their info" do
+    visit root_path
+    fill_in "sign_in_first_name", with: user.first_name
+    fill_in "sign_in_email", with: user.email
+    fill_in "sign_in_password", with: user.password
+    click_button("Sign In")
+    click_link("Update Info")
+    fill_in "user_first_name", with: "Steveo"
+    click_button("Update Info")
+    expect(page).to have_content("Welcome, Steveo!")
+  end
+end
