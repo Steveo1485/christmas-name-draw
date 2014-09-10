e15bcc6fe5d30e5a43308e48a12dcf0fcc33fc09
steveo1485 <stevennugent@gmail.com>
2014-09-10 10:45:51 -0700

    Move database_cleaner config into support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,27 +14,6 @@ SimpleCov.start
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = false
